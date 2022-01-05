e6823c374f72d4166845cec9352788522b69ffea
Juan Ignacio Rios <juanrios0495@gmail.com>
2022-01-04 23:11:43 -0300

    Edit some stuff in Chapter 1. Start Chapter 2

---
+++ chapter2/zombieFactory.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.25;
+
+contract ZombieFactory {
+
+    event NewZombie(
+        uint zombieId, 
+        string name, 
+        uint dna
+    );
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // Mapping address
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        
+        // save 'msg.sender' by 'id'
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
